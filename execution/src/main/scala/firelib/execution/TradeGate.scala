4694f45375a71aee42221cff53a99269cded68b5
ivan <ivan.antipin@gmail.com>
2014-09-16 08:20:17 +0400

    some docs

--- execution/src/main/scala/firelib/execution/TradeGate.scala
+++ execution/src/main/scala/firelib/execution/TradeGate.scala
@@ -1,17 +1,36 @@
 package firelib.execution
 
-import firelib.common.{TradeGateCallback, _}
 import firelib.common.threading.ThreadExecutor
+import firelib.common.{TradeGateCallback, _}
 
+/**
+ * Main interface for execution to adapt broker api.
+ */
 trait TradeGate {
 
+    /**
+     * just order send
+     */
     def sendOrder(order: Order)
 
+    /**
+     * just order cancel by id
+     */
     def cancelOrder(orderId: String)
 
+    /**
+     * register callback to receive notifications about trades and order statuses
+     */
     def registerCallback(tgc: TradeGateCallback)
 
+    /**
+     * pass configuration params to gate
+     * usually it is user/password, broker port and url etc
+     */
     def configure(config: Map[String, String], callbackExecutor: ThreadExecutor)
 
+    /**
+     * need to run start to finish initialization
+     */
     def start()
 }
--- execution/src/main/scala/firelib/execution/config/ModelRuntimeConfig.scala
+++ execution/src/main/scala/firelib/execution/config/ModelRuntimeConfig.scala
@@ -3,16 +3,37 @@ package firelib.execution.config
 import firelib.common.config.ModelConfig
 
 
+/**
+ * configuration for execution environment
+ */
 class ModelRuntimeConfig {
 
+    /**
+     * model config, same as used for backtesting
+     * to init model on start
+     */
     var modelConfig: ModelConfig = new ModelConfig()
 
+    /**
+     * class of adapter, will be created with reflection
+     * adapter need to implement TradeGate and MarketDataProvider
+     */
     var gatewayType: String = _
 
+    /**
+     * specify if model need to run backtest before start as often we need to init
+     * different kinds of quantiles etc
+     */
     var runBacktest: Boolean = false
 
+    /**
+     * config passed to trading gateway
+     */
     var gatewayConfig: Map[String, String]=_
 
+    /**
+     * directory to log trades
+     */
     var tradeLogDirectory: Option[String]=_
 
 }
